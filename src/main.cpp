35d3d807ac604f747c21d234357fa93532dce735
tilt377-3 <wardroiddarkness@gmail.com>
2025-06-08 19:38:33 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,26 @@
 // Copyright 2022 NNTU-CS
-#include "tree.h"
 #include <iostream>
+#include <vector>
+#include "tree.h"
+
 int main() {
-  std::vector<char> in = { '1','2','3' };
+  std::vector<char> in = {'1', '2', '3'};
   PMTree tree(in);
-  
   std::cout << "All permutations:\n";
   auto allPerms = getAllPerms(tree);
   for (const auto& perm : allPerms) {
     for (char c : perm) std::cout << c;
     std::cout << "\n";
   }
-  
   std::cout << "\nPermutation 1: ";
   auto perm1 = getPerm1(tree, 1);
   for (char c : perm1) std::cout << c;
   std::cout << "\n";
-  
   std::cout << "Permutation 2: ";
   auto perm2 = getPerm2(tree, 2);
   for (char c : perm2) std::cout << c;
   std::cout << "\n";
-  
   std::cout << "\nRunning experiment...\n";
   runExperiment();
-  
   return 0;
 }
